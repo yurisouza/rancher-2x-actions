cabee1d592a54addfc3311dcf58b15712935e55c
Yuri Pereira <contato.yuripereira@gmail.com>
2022-04-12 00:26:59 -0300

    Setting name

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ async function main() {
   const rancherUrl = core.getInput('rancher_url', {required: true});
   const rancherToken = core.getInput('rancher_token', {required: true});
   const clusterId = core.getInput('cluster_id', {required: true});
-  const projectId = core.getInput('project_id', {required: true});
   const namespace = core.getInput('namespace', {required: true});
   const deployment = core.getInput('deployment', {required: true});
   const dockerImage = core.getInput('docker_image', {required: true});
@@ -25,17 +24,6 @@ async function main() {
     {
       headers: {
         'Content-Type': 'application/json-patch+json',
-        'Authorization': 'Bearer ' + rancherToken,
-      },
-    },
-  );
-
-  await axios.post(
-    `${rancherUrl}/v3/projects/${clusterId}:${projectId}/workloads/deployment:${namespace}:${deployment}?action=redeploy`,
-    {},
-    {
-      headers: {
-        'Content-Type': 'application/json',
         'Authorization': 'Bearer ' + rancherToken,
       },
     },
